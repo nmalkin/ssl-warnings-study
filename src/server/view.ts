df9752bfd72bc239e9a7f594aea2882aeb9745c9
Nathan Malkin <nmalkin@cs.berkeley.edu>
2016-05-26 01:09:58 +0000

    Fix rendering of new Firefox error page

--- src/server/view.ts
+++ src/server/view.ts
@@ -13,6 +13,10 @@ export function warning(browser : browser_detect.Browser, condition : conditions
     if(browser === browser_detect.Browser.Other) {
         target(res);
         return;
+    } else if(browser === browser_detect.Browser.FirefoxNew) {
+        // The new Firefox error page is written in XHTML. Without the correct
+        // content-type, it won't render properly.
+        res.set('Content-Type', 'application/xhtml+xml');
     }
 
     var view = path.join('warnings', browser_detect.filenameForBrowser(browser));
